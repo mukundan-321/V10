5474edbdc4c523406071a453a5fe0ef51a25be4b
mukundan-321 <mukundan1014@gmail.com>
2026-07-25 22:07:42 +0530

    Update app_database.dart

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -5,7 +5,7 @@ import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart';
-import 'package:sqlite3/sqlite3.dart';
+
 
 import 'tables/messages_table.dart';
 import 'tables/reactions_table.dart';
